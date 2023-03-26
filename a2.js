df2e0b303f89a8330223f0a704e52b0b1cc5f3cc
Mrinmoy Bordoloi <86611284+MRINMOY3@users.noreply.github.com>
2023-03-26 13:33:54 +0530

    Update a2.js

--- a2.js
+++ a2.js
@@ -1,5 +1,14 @@
+
+
+
 "use-strict";
 
+navigator.vibrate(200); // vibrate for 200ms
+navigator.vibrate([
+  100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
+
+
+]); // Vibrate 'SOS' in Morse.
 let g = "Hello";
 console.log(typeof g);
 console.log(g);
